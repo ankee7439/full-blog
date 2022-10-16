52e48fe19eba10a9ceea33b54748192afb125097
ankee7439 <88872056+ankee7439@users.noreply.github.com>
2022-10-16 20:12:19 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,86 @@
+
+
+    <!-- Now Come and write something about the Category -->
+
+    <section class="category__buttons">
+        <div class="container category__button-container">
+            <a href="" class="category__button">Category</a>
+            <a href="" class="category__button">Art</a>
+            <a href="" class="category__button">Wild Life</a>
+            <a href="" class="category__button">Travel</a>
+            <a href="" class="category__button">Science & Technology</a>
+            <a href="" class="category__button">Food</a>
+            <a href="" class="category__button">Music</a>
+        </div>
+    </section>
+<!-- ENDING OF CATEGORY BUTTONS -->
+
+<!-- NOW LETS MOVE ON THE NEXT TOPIC WHICH IS FOOTER PART -->
+
+<footer>
+    <div class="footer__socials">
+        <a href="" target="_blank"><i class="uil uil-youtube"></i></a>
+        <a href="" target="_blank"><i class="uil uil-facebook"></i></a>
+        <!-- <a href="" target="_blank"><i class="fa-brands fa-facebook"></i></a> -->
+        <a href="" target="_blank"><i class="uil uil-instagram-alt"></i></a>
+        <a href="" target="_blank"><i class="uil uil-linkedin"></i></a>
+
+    </div>
+    <div class="container footer__container">
+        <article>
+            <h4>Categories</h4>
+            <ul>
+                <li><a href="">Art</a></li>
+                <li><a href="">Wild Life</a></li>
+                <li><a href="">Travel</a></li>
+                <li><a href="">Science & Technology</a></li>
+                <li><a href="">Food</a></li>
+                <li><a href="">Music</a></li>
+            </ul>
+        </article>
+        <article>
+            <h4>Categories</h4>
+            <ul>
+                <li><a href="">Online Support</a></li>
+                <li><a href="">Call Number</a></li>
+                <li><a href="">Emails</a></li>
+                <li><a href="">Socials Support</a></li>
+                <li><a href="">Location</a></li>
+                
+            </ul>
+        </article>
+        <article>
+            <h4>Categories</h4>
+            <ul>
+                <li><a href="">Safety</a></li>
+                <li><a href="">Repair</a></li>
+                <li><a href="">Recent</a></li>
+                <li><a href="">Popular</a></li>
+                <li><a href="">Categories</a></li>
+                
+            </ul>
+        </article>
+         <article>
+            <h4>Categories</h4>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Blog</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Services</a></li>
+                <li><a href="">Contact</a></li>
+                
+            </ul>
+        </article>
+    </div>
+
+    <div class="footer__copyright">
+        <small>Copyright &copy; 2022 Programming with ankit</small>
+    </div>
+</footer>
+
+
+<!-- NOW ADD SOME JS INTO IT -->
+<script src="app.js"></script>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AWESOME BLOG</title>
+    <link rel="stylesheet" href="app.css">
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <!-- Montserrat fonts -->
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,600;0,900;1,500&display=swap"
+        rel="stylesheet">
+</head>
+
+<body>
+    <!-- NavBar Starting -->
+    <nav>
+        <div class="container nav__container">
+            <a href="index.html" class="nav__logo">AWESOME BLOG</a>
+            <ul class="nav__items">
+                <li><a href="blog.html">Blog</a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="services.html">Services</a></li>
+                <li><a href="contact.html">Contact</a></li>
+                <li><a href="signin.html">Sing In</a></li>
+                <li class="nav__profile">
+                    <div class="avatar">
+                        <img src="./images/avatar1.jpg">
+                    </div>
+                    <ul>
+                        <li><a href="dashboard.html">Dashboard</a></li>
+                        <li><a href="logout.html">Log Out</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <button id="open__nav-btn"><i class="uil uil-bars"></i></button>
+            <button id="close__nav-btn"><i class="uil uil-multiply"></i></button>
+        </div>
+    </nav>
+    <!-- NavBar Ending -->
+    <!-- THIS PAGE WILL SHOW THE SINGLE POST  -->
+    
